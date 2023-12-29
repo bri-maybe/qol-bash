085d757d98e8fcc2d30c22dbccb8e849a6dc92de
bober <brian.familybrian123@gmail.com>
2023-12-29 19:32:11 +0900

    fix(mv func): prevent mv of identical files

--- sweep.sh
+++ sweep.sh
@@ -1,7 +1,9 @@
-directory="/Users/bober/Downloads"
-imgpath="/Users/bober/Downloads/Images"
-docpath="/Users/bober/Downloads/Docs"
-miscpath="/Users/bober/Downloads/Misc"
+#!/opt/homebrew/share/man/man1/bash.1
+
+directory="$HOME/Downloads"
+imgpath="$HOME/Downloads/Images"
+docpath="$HOME/Downloads/Docs"
+miscpath="$HOME/Downloads/Misc"
 ignored=""
 
 if [ ! -d "$imgpath" ]; then
@@ -23,18 +25,23 @@ sweepdir() {
             echo $filename
             continue
         fi
-        filepath="$directory/$filename"
+        filepath="$1/$filename"
         if [[ -d "$filepath" ]]; then
             continue
         fi
         fileformat=$(file -I "$filepath" | awk -F '[:;]' '{print $2}')
         if [[ $(echo $fileformat | cut -f 1 -d '/') == "image" ]];then
-            mv "$filepath" "$imgpath"
+            if [[ "$1" != "$imgpath" ]]; then
+                mv "$filepath" "$imgpath"
+            fi
         elif [[ $(echo $fileformat | cut -f 1 -d '/') == "text" ]];then #TODO add more file formats
-            mv "$filepath" "$docpath"
+            if [[ "$1" != "$docpath" ]]; then
+                mv "$filepath" "$docpath"
+            fi
         else
-            mv "$filepath" "$miscpath"
-            
+            if [[ "$1" != "$miscpath" ]]; then
+                mv "$filepath" "$miscpath"
+            fi
         fi
     done < <(ls $1)  # Change the find command options as needed
 }
@@ -42,5 +49,6 @@ sweepdir() {
 sweepdir "$directory"
 sweepdir "$imgpath"
 sweepdir "$docpath"
+sweepdir "$miscpath"
 
 
